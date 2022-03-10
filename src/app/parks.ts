63f1633b1177b35eafd5854322a098734b9a53e5
mdlawrenceusa <mdlawrence@me.com>
2022-03-10 16:57:50 -0500

    2 files modified, 5 added

---
+++ src/app/parks.ts
@@ -0,0 +1,16 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+  },
+
+];
